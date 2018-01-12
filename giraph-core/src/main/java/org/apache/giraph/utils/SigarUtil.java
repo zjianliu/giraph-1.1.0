7c322088b64630044ddf5e85a07b73c8ece582c6
zjianliu <1136372287@qq.com>
2018-01-12 16:08:52 +0800

    fix some bugs in SigarUtil.java

--- giraph-core/src/main/java/org/apache/giraph/utils/SigarUtil.java
+++ giraph-core/src/main/java/org/apache/giraph/utils/SigarUtil.java
@@ -18,33 +18,29 @@ public class SigarUtil {
 
     public static Sigar getSigar() throws IOException{
         final Logger LOG = Logger.getLogger(SigarUtil.class);
-        String ResourceSigarFolderName = "sigar_lib";
-        URL sigarFolderNameURL = Resources.getResource(ResourceSigarFolderName);
-        File sigarFolder = new File(sigarFolderNameURL.getFile());
 
-        if (!sigarFolder.exists()) {
+        String sigarFolderName = "/home/hadoop/lib/sigar_lib";
+        File sigarFolder = new File(sigarFolderName);
+
+        if(!sigarFolder.exists()){
             sigarFolder.mkdir();
 
-            File file = new File(Resources.getResource("sigar_lib.zip").getFile());
-            ZipFile sigarZip = new ZipFile(file);
-
-            ZipInputStream zis = new ZipInputStream(new FileInputStream(file));
+            InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("sigar_lib.zip");
+            ZipInputStream zis = new ZipInputStream(is);
 
             ZipEntry entry;
             while ((entry = zis.getNextEntry()) != null) {
                 System.out.println("decompress file :" + entry.getName());
-                File outFile = new File(sigarFolderNameURL.getFile() + entry.getName());
-                BufferedInputStream bis = new BufferedInputStream(sigarZip.getInputStream(entry));
+                File outFile = new File(sigarFolderName + "/" + entry.getName());
                 BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(outFile));
 
                 byte[] buffer = new byte[1024];
                 while (true) {
-                    int len = bis.read(buffer);
+                    int len = zis.read(buffer);
                     if (len == -1)
                         break;
                     bos.write(buffer, 0, len);
                 }
-                bis.close();
                 bos.close();
             }
             zis.close();
@@ -60,10 +56,7 @@ public class SigarUtil {
         System.setProperty("java.library.path", path);
         LOG.info("SigarUtil: java.library.path added!");
         LOG.info("SigarUtil: After set java.library.path, it is " + System.getProperty("java.library.path"));
-        if( (new File(sigarFolder.getCanonicalPath() + "/libsigar-amd64-linux.so")).exists())
-            LOG.info(sigarFolder.getCanonicalPath() + "exists!");
-        else
-            LOG.info(sigarFolder.getCanonicalPath() + "does not exist!");
+        System.load(sigarFolder.getCanonicalPath() + "/libsigar-amd64-linux.so");
         return new Sigar();
     }
 }
